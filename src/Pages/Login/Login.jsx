594399f48888f3add63952d39312fca3f767357d
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-11-25 00:39:57 +0100

    authentication done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,12 +2,30 @@ import login from '../../assets/Resources/login/login.jpg';
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const Login = () => {
 
+    const { signIn } = useContext(AuthContext);
+
     const handleLogin = event => {
         event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
 
+        console.log(email, password);
+
+        signIn(email, password)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
     }
     return (
--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,10 +1,30 @@
+import { useContext } from 'react';
 import login from '../../assets/Resources/login/login.jpg';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const Registration = () => {
+
+    const { createUser } = useContext(AuthContext);
+
     const handleRegister = event => {
         event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const photo = form.photo.value;
+        console.log(name, email, password, photo)
 
+        createUser(email, password)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
     }
     return (
@@ -16,13 +36,13 @@ const Registration = () => {
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                        <h1 className="text-3xl text-center font-bold">Login</h1>
+                        <h1 className="text-3xl text-center font-bold">Registration</h1>
                         <form onSubmit={handleRegister}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="email" name='name' placeholder="name" className="input input-bordered" required />
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
@@ -41,7 +61,7 @@ const Registration = () => {
                                 <label className="label">
                                     <span className="label-text">Photo URL</span>
                                 </label>
-                                <input type="password" name='photo' placeholder="photo url" className="input input-bordered" required />
+                                <input type="text" name='photo' placeholder="photo url" className="input input-bordered" required />
 
                             </div>
                             <div className="form-control mt-6">
