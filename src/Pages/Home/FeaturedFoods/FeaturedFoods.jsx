bd1d0daaf6c5a6fb5d8ca9ae616251102d4f2922
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-11-27 19:43:54 +0100

    added show all button  that will redirect to the Available Foods page

--- src/Pages/Home/FeaturedFoods/FeaturedFoods.jsx
+++ src/Pages/Home/FeaturedFoods/FeaturedFoods.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import FeaturedFoodsCard from "./FeaturedFoodsCard";
-import { useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
+//import { useLoaderData } from "react-router-dom";
 
 
 
 
 const FeaturedFoods = () => {
     const [featuredFoods, setFeaturedFoods] = useState([]);
-    const [itemsPerPage, setItemsPerPage] = useState(6);
 
-    const { count } = useLoaderData();
+    const [dataLength, setDataLength] = useState(6);
+    //const [currentPage, setCurrentPage] = useState(0);
+    //const [itemsPerPage, setItemsPerPage] = useState(6);
 
-    const numberOfPages = Math.ceil(count / itemsPerPage);
+    //const { count } = useLoaderData();
+
+    //const numberOfPages = Math.ceil(count / itemsPerPage);
 
     //ONE WAY FOR PAGINATION
 
@@ -23,14 +27,29 @@ const FeaturedFoods = () => {
 
     //ANOTHER WAY
 
-    const pages = [...Array(numberOfPages).keys()];
+    //const pages = [...Array(numberOfPages).keys()];
 
-    const handleItemsPerPageChange = e => {
+    {/*const handleItemsPerPageChange = e => {
 
         const val = parseInt(e.target.value);
         console.log(val);
         setItemsPerPage(val);
+        setCurrentPage(0);
     }
+
+    const handlePrevPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+
+    }
+
+    const handleNextPage = () => {
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+
+    } */}
 
     useEffect(() => {
         fetch('http://localhost:5000/featuredFoods')
@@ -49,23 +68,32 @@ const FeaturedFoods = () => {
                 <p>feature foods: {featuredFoods.length}</p>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
                     {
-                        featuredFoods.map(featuredFood => <FeaturedFoodsCard
+                        featuredFoods.slice(0, dataLength).map(featuredFood => <FeaturedFoodsCard
                             key={featuredFood._id}
                             featuredFood={featuredFood}
                         ></FeaturedFoodsCard>)
                     }
                 </div>
             </div>
-            <div className="text-center mt-4">
+            {/*   <div className="text-center mt-4  ">
+                <p>current page: {currentPage}</p>
+                <button onClick={handlePrevPage} className="btn mr-2">Prev</button>
                 {
-                    pages.map(page => <button className="btn text-center mr-2" key={page}>{page}</button>)
+                    pages.map(page => <button
+                        onClick={() => setCurrentPage(page)}
+                        className="btn text-center mr-2 " key={page}>{page}</button>)
                 }
+                <button onClick={handleNextPage} className="btn">Next</button>
                 <select value={itemsPerPage} onChange={handleItemsPerPageChange} name="" id="">
                     <option value="6">6</option>
                     <option value="12">12</option>
                     <option value="18">18</option>
                 </select>
 
+            </div>*/ }
+            <div className="text-center dataLength === featuredFoods.length ? 'hidden' : '' ">
+                <Link to="/AvailableFoods"
+                    onClick={() => { setDataLength(featuredFoods.length) }} className="btn btn-accent mt-4 ">Show All</Link>
             </div>
 
         </div>
