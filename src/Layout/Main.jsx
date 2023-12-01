48adcf75e2a8923f0c2090ec556b4db4560dc755
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-12-01 12:17:21 +0100

    Another Extra featured added in the home page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
 import NavBar from './../Pages/Shared/NavBar/NavBar';
 import ExtraFeatured1 from "../Pages/Home/ExtraFeatured1/ExtraFeatured1";
+import ExtraFeatured2 from "../Pages/Home/ExtraFeatured2/ExtraFeatured2";
 
 
 
@@ -11,6 +12,7 @@ const Main = () => {
             <NavBar></NavBar>
             <Outlet></Outlet>
             <ExtraFeatured1></ExtraFeatured1>
+            <ExtraFeatured2></ExtraFeatured2>
             <Footer></Footer>
         </div>
     );
---
+++ src/Pages/Home/ExtraFeatured2/ExtraFeatured2.jsx
@@ -0,0 +1,20 @@
+
+
+const ExtraFeatured2 = () => {
+    return (
+        <div className="bg-teal-100  rounded-lg mt-8">
+            <h2 className="text-center font-bold text-teal-500 text-3xl pt-12">
+                It's fast, simple, and hassle-free!</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-2 mt-6 ">
+                <p className=" ml-60">Your community needs you! Interested? Get <br /> involved! FoodSHare platform is an initiative <br /> under project PLOUTOS and will be active in <br /> two Blakan countries, Serbia and North <br /> Macedonia.</p>
+                <p className="mr-60">
+                    On FoodShare, feel free to share any <br /> perishable or frozen items like dairy, fish, <br /> meat, and fresh juice as long as they are <br /> within or before their 'use by date' and still <br /> suitable for human consumption. <br /> Additionally, non-perishable food items are <br /> welcome as long as they are within their <br /> 'best before date'.</p>
+            </div>
+            <div className="text-center mt-6 ">
+                <button className="btn rounded-2xl mb-6 text-teal-500 ">Join Us</button>
+            </div>
+        </div>
+    );
+};
+
+export default ExtraFeatured2;
